3213dc6cba1a9278db3db3e0d3024c9b91948ed3
switch <3440501841@qq.com>
2025-03-23 15:28:38 +0800

    受不了了我要更改我的ssh配置

--- src/views/sandbox/home/Home.jsx
+++ src/views/sandbox/home/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button } from 'antd'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      Home
+      <Button type="primary" ghost>
+        Primary
+      </Button>
+    </div>
   )
 }

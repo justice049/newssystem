e7af992f37174cae321a3cafbc4a0f69a7ea2e8a
switch <3440501841@qq.com>
2025-03-24 17:19:03 +0800

    更新依赖

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
-import React,{useEffect} from "react"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import style from './child.module.scss'
-import axios from 'axios'
-import IndexRouter from "./router/IndexRouter"
-import NewsSandBox from "./views/sandbox/NewsSandBox"
 
 function App() {
-  // useEffect(() => {
-  //   axios
-  //     .get(
-  //       'http://localhost:3001/api/mmdb/movie/v3/list/hot.json?ct=西安&ci=42&channelId=4'
-  //     )
-  //     .then((res) => {
-  //       console.log(res.data)
-  //     })
-  //     .catch((error) => {
-  //       console.error('请求出错:', error)
-  //     })
-  // }, [])
-
   return (
-    // <IndexRouter/>
-    <NewsSandBox/>
+    <div>
+      app
+      <ul>
+        <li className={style.list}></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <li>111</li>
+      <li>111</li>
+    </div>
   )
 }
 
